be88a6e5b0f59c246a98aa913edc575e2d23ce0a
kcanaan <kiaracanaan@gmail.com>
2022-03-01 14:07:18 -0500

    added search form

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>DICTIONARY</h1>
+        <Dictionary />
       </header>
     </div>
   );
