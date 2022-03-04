384bbcf6a2a6e62c81c1b8ae3159b38241e32b96
kcanaan <kiaracanaan@gmail.com>
2022-03-04 13:03:14 -0500

    fixed deploy

--- src/Photos.js
+++ src/Photos.js
@@ -13,7 +13,7 @@ export default function Photos(props) {
                 <img
                   src={photo.src.landscape}
                   className="img-fluid"
-                  alt="image"
+                  alt="definition"
                 />
               </div>
             );
